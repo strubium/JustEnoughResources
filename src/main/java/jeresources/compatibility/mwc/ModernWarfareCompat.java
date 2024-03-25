2e1f48f6487fb914b637a3b7c28ece6110461c1a
strubium <113206902+strubium@users.noreply.github.com>
2024-03-25 12:56:02 -0500

    Update ModernWarfareCompat.java

--- src/main/java/jeresources/compatibility/mwc/ModernWarfareCompat.java
+++ src/main/java/jeresources/compatibility/mwc/ModernWarfareCompat.java
@@ -16,11 +16,11 @@ import jeresources.entry.WorldGenEntry;
 import jeresources.util.LootTableHelper;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemSeedFood;
-import net.minecraft.item.ItemSeeds;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.loot.LootTableManager;
+import com.paneedah.mwc.init.MWCBlocks;
+import com.paneedah.mwc.init.MWCItems;
 
 public class ModernWarfareCompat extends CompatBase {
     @Override
@@ -39,14 +39,10 @@ public class ModernWarfareCompat extends CompatBase {
     }
 
     private void registerOres() {
-        registerWorldGen(new WorldGenEntry(new ItemStack(Blocks.LAPIS_ORE), new DistributionTriangular(15, 15, 0.001F), true, new LootDrop(new ItemStack(Items.DYE, 4, 4))));
-        registerWorldGen(new WorldGenEntry(new ItemStack(Blocks.IRON_ORE), new DistributionSquare(20, 8, 1, 64)));
-        registerWorldGen(new WorldGenEntry(new ItemStack(Blocks.REDSTONE_ORE), new DistributionSquare(8, 7, 1, 16), true, new LootDrop(new ItemStack(Items.REDSTONE, 4))));
-        registerWorldGen(new WorldGenEntry(new ItemStack(Blocks.DIAMOND_ORE), new DistributionSquare(1, 7, 1, 16), true, new LootDrop(new ItemStack(Items.DIAMOND))));
-        registerWorldGen(new WorldGenEntry(new ItemStack(Blocks.EMERALD_ORE), new DistributionSquare(6, 1, 4, 32), new Restriction(BiomeRestriction.EXTREME_HILLS), true, new LootDrop(new ItemStack(Items.EMERALD))));
-        registerWorldGen(new WorldGenEntry(new ItemStack(Blocks.GOLD_ORE), new DistributionSquare(2, 8, 1, 32)));
-        registerWorldGen(new WorldGenEntry(new ItemStack(Blocks.COAL_ORE), new DistributionSquare(20, 16, 1, 128), true, new LootDrop(new ItemStack(Items.COAL))));
-        registerWorldGen(new WorldGenEntry(new ItemStack(Blocks.QUARTZ_ORE), new DistributionSquare(20, 14, 1, 126), new Restriction(DimensionRestriction.NETHER), true, new LootDrop(new ItemStack(Items.QUARTZ, 4))));
-        registerWorldGen(new WorldGenEntry(new ItemStack(Blocks.CLAY), new DistributionUnderWater(0.0035F), new LootDrop(new ItemStack(Items.CLAY_BALL, 4))));
+        registerWorldGen(new WorldGenEntry(new ItemStack(MWCBlocks.copperOre), new DistributionSquare(15, 15, 64)));
+        registerWorldGen(new WorldGenEntry(new ItemStack(MWCBlocks.tinOre), new DistributionSquare(20, 8, 1, 64)));
+        registerWorldGen(new WorldGenEntry(new ItemStack(MWCBlocks.leadOre), new DistributionSquare(8, 7, 1, 16)));
+        registerWorldGen(new WorldGenEntry(new ItemStack(MWCBlocks.sulfurOre), new DistributionSquare(1, 7, 1, 16), true, new LootDrop(new ItemStack((MWCItems.sulfurDust, 2, 5))));
+        registerWorldGen(new WorldGenEntry(new ItemStack(MWCBlocks.graphiteOre), new DistributionSquare(6, 1, 4, 32), true, new LootDrop(new ItemStack(MWCItems.graphiteChunk, 1, 3))));
     }
 }
